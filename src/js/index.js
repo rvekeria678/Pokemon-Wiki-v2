66ba79e59a16f88630b4bf5897c179a6da2c8d9d
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2023-03-01 10:51:06 -0500

    Added Side Content Component

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,13 @@ const input = document.querySelector('#input');
 
 const next = document.querySelector('#next');
 const prev = document.querySelector('#prev');
+const back = document.querySelector('#back');
 const pokemon__content = document.querySelector('#Pokedex');
+
+const main__content = document.querySelector('#MainContent');
+const side__content = document.querySelector('#SideContent');
+
+//side__content.style.display = 'none';
 
 let next_url = '', prev_url = '';
 
@@ -43,6 +49,10 @@ prev.addEventListener('click', (event)=>{
     pokemon__content.innerText = '';
     getBatch(prev_url);
 });
+back.addEventListener('click', (event)=>{
+    side__content.style.display = 'none';
+    main__content.removeAttribute("style");
+});
 
 function newCard(data, wrapper) {
     // Creating HTML Elements
@@ -61,7 +71,29 @@ function newCard(data, wrapper) {
         card__types.append(type);
     }
     card__image.src = data.sprites.front_default;
+
+    // Side Content Handler
     card__more.innerText = 'Read More';
+    card__more.addEventListener('click', (event)=> {
+        side__content.removeAttribute("style");
+        main__content.style.display = 'none';
+
+        const side__image = document.querySelector('#side--img');
+        const side__title = document.querySelector('#side--title');
+        const side__type = document.querySelector("#side--type");
+
+        side__type.innerText = '';
+
+        side__image.src = data.sprites.front_default;
+        side__title.innerText = data.name;
+
+        for (let i = 0; i < data.types.length; ++i) {
+            const type = document.createElement('div');
+            type.innerText = data.types[i].type.name;
+            type.className = 'mr-3 my-2 border px-3 py-1 rounded-full w-auto text-center text-white font-semibold ' + type__colors[type.innerText];
+            side__type.append(type);
+        }
+    });
     // Styling HTML elements
     card.className = 'flex justify-between shadow rounded-2xl m-3 p-4 bg-white';
     card__title.className = 'text-3xl font-Phudu';

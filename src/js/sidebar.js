433adc5aedac9f2834ae43155ac1d561d1ce2c6b
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2023-03-03 09:44:29 -0500

    Added working sidebar nav

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,20 +1,22 @@
-const toggler = document.querySelector('#sidebar-toggle');
+const toggler = document.querySelectorAll('.sidebar-toggle');
 
-toggler.addEventListener('click',(event)=>{
-    sidebar = document.querySelector('#sidebar');
-    links = document.querySelector('#sidebar-links');
-
-    if (sidebar.classList.contains('w-0')) {
-        sidebar.classList.remove('w-0');
-        sidebar.classList.add('w-80')
-        sidebar.classList.add('z-40');
-        links.classList.add('visible');
-        links.classList.remove('invisible');
-    } else {
-        sidebar.classList.remove('w-80');
-        sidebar.classList.add('w-0');
-        sidebar.classList.remove('z-40');
-        links.classList.remove('visible');
-        links.classList.add('invisible');
-    }
-});
+toggler.forEach((e=>{
+    e.addEventListener('click', (event)=>{
+        sidebar = document.querySelector('#sidebar');
+        links = document.querySelector('#sidebar-links');
+    
+        if (sidebar.classList.contains('w-0')) {
+            sidebar.classList.remove('w-0');
+            sidebar.classList.add('w-80')
+            //sidebar.classList.add('z-40');
+            links.classList.add('visible');
+            links.classList.remove('invisible');
+        } else {
+            sidebar.classList.remove('w-80');
+            sidebar.classList.add('w-0');
+            //sidebar.classList.remove('z-40');
+            links.classList.remove('visible');
+            links.classList.add('invisible');
+        }
+    });
+}));
